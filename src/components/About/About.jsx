59deee1e7c3bde543aefd1a9ad805af16b6bbdaf
MrHogun KSU <[s.j.000abk@gmail.com]>
2024-10-16 15:55:15 +0300

    feat(about): add about module with basic styles

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,14 +1,43 @@
 import React from "react";
+
+import styles from "./About.module.css";
 import { getImageUrl } from "../../utils";
 
 const About = () => {
   return (
-    <section>
-      <h2>About</h2>
-      <div>
-        <img src={getImageUrl("about/adoutImage.png")} alt="About image" />
-        <ul>
-          <li></li>
+    <section className={styles.container}>
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/adoutImage.png")}
+          alt="About image"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/")} alt="" />
+            <div className={styles.aboutItemText}>
+              <h3>Frontend</h3>
+              <p>I study front-end development, now I'm learning React</p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/")} alt="" />
+            <div className={styles.aboutItemText}>
+              <h3>Pixelarts</h3>
+              <p>I can draw a little in the pixel art style</p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/")} alt="" />
+            <div className={styles.aboutItemText}>
+              <h3>Graphic editors</h3>
+              <p>
+                I am proficient in graphic editors such as Photoshop Illustrator
+                and others
+              </p>
+            </div>
+          </li>
         </ul>
       </div>
     </section>
