75885131350769bd49be7ee254cde2966ffa99f5
MrHogun KSU <[s.j.000abk@gmail.com]>
2024-10-18 13:02:44 +0300

    feat(experience): add experience module and styles

---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { getImageUrl } from "../../utils";
+
+import styles from "./Experience.module.css";
+
+const Experience = () => {
+  return <section></section>;
+};
+
+export default Experience;
