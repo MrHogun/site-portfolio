3c96aef6cb9520288f3f9c1cd6b524b1c29a03d3
MrHogun KSU <[s.j.000abk@gmail.com]>
2024-10-18 13:16:36 +0300

    feat(data): add new entires to skills.json and history.json

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -4,7 +4,15 @@ import { getImageUrl } from "../../utils";
 import styles from "./Experience.module.css";
 
 const Experience = () => {
-  return <section></section>;
+  return (
+    <section id="experience">
+      <h2>Experience</h2>
+      <div>
+        <div></div>
+        <ul></ul>
+      </div>
+    </section>
+  );
 };
 
 export default Experience;
