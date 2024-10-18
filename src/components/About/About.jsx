cbc8c32014f8ba218f2fbd5c33a20481b02b1895
MrHogun KSU <[s.j.000abk@gmail.com]>
2024-10-18 12:50:00 +0300

    feat(about): rewrite about styles, add images

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,27 +9,27 @@ const About = () => {
       <h2 className={styles.title}>About</h2>
       <div className={styles.content}>
         <img
-          src={getImageUrl("about/adoutImage.png")}
+          src={getImageUrl("about/chest.svg")}
           alt="About image"
           className={styles.aboutImage}
         />
         <ul className={styles.aboutItems}>
           <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/")} alt="" />
+            <img src={getImageUrl("about/frontEnd.svg")} alt="Pixelart key" />
             <div className={styles.aboutItemText}>
               <h3>Frontend</h3>
               <p>I study front-end development, now I'm learning React</p>
             </div>
           </li>
           <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/")} alt="" />
+            <img src={getImageUrl("about/pixelArt.svg")} alt="" />
             <div className={styles.aboutItemText}>
               <h3>Pixelarts</h3>
               <p>I can draw a little in the pixel art style</p>
             </div>
           </li>
           <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/")} alt="" />
+            <img src={getImageUrl("about/graphicDesign.svg")} alt="" />
             <div className={styles.aboutItemText}>
               <h3>Graphic editors</h3>
               <p>
