6189427bad75d7f45d6a2ee63f496d84bde96f6b
MrHogun KSU <[s.j.000abk@gmail.com]>
2024-10-18 15:29:29 +0300

    feat(carousel): update basic carousel files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import styles from "./App.module.css";
 import About from "./components/About/About";
-import Experience from "./components/Experience/Experience";
 import Hero from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/NavBar";
 
@@ -10,7 +9,6 @@ function App() {
       <Navbar />
       <Hero />
       <About />
-      <Experience />
     </div>
   );
 }
---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./Carousel.module.css";
+
+const Carousel = ({ data }) => {
+  return <div>Carousel Component</div>;
+};
+
+export default Carousel;
